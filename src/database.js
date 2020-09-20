b5aa1398cf60449d5d2e06d292f430cdff6f0d45
Fernando Castro <fc93435@gmail.com>
2020-09-20 02:52:12 -0300

    make it better

--- src/database.js
+++ src/database.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
+const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
+const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
 
-mongoose.connect('mongodb://localhost/id-app', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
 });
 
 const db = mongoose.connection;
